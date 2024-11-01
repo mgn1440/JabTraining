07079f4c033b96877a81fd0ed77dbb57b70c955c
mgn1440 <mgn1440@gmail.com>
2024-11-01 14:50:15 +0900

    fix: appbar color change when scroll close #96

--- lib/pages/reservation_page.dart
+++ lib/pages/reservation_page.dart
@@ -23,21 +23,21 @@ class _ReservationsPageState extends State<ReservationsPage> {
   }
 
   void _loadReservations() {
-      _reservationsByDate = fetchReservationsGroupedByDate();
+    _reservationsByDate = fetchReservationsGroupedByDate();
   }
 
   Future<void> _cancelReservation(String reservationId) async {
     try {
-       await supabase
+      await supabase
           .from('reservations')
           .delete()
           .eq('workout_id', reservationId);
-       setState(() {
-         _loadReservations(); // 상태 업데이트를 위해 setState를 사용
-       });
-       if (mounted) {
-         context.showSnackBar('예약이 취소되었습니다.', isError: false);
-       }
+      setState(() {
+        _loadReservations(); // 상태 업데이트를 위해 setState를 사용
+      });
+      if (mounted) {
+        context.showSnackBar('예약이 취소되었습니다.', isError: false);
+      }
     } catch (e) {
       if (mounted) {
         context.showSnackBar('예약 취소 중 오류가 발생했습니다.', isError: true);
@@ -71,18 +71,19 @@ class _ReservationsPageState extends State<ReservationsPage> {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                     AppBar(
-                       backgroundColor: background,
-                       title: Text(
-                           _formatDate(date),
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                       ),
-                       automaticallyImplyLeading: false,
-                     ),
+                    AppBar(
+                      backgroundColor: background,
+                      scrolledUnderElevation: 0,
+                      title: Text(
+                        _formatDate(date),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      automaticallyImplyLeading: false,
+                    ),
                     ListView.builder(
                         shrinkWrap: true,
                         physics: const NeverScrollableScrollPhysics(),
@@ -96,20 +97,17 @@ class _ReservationsPageState extends State<ReservationsPage> {
                             onReserve: () => _cancelReservation(reservation.id),
                             isReservationPage: true,
                           );
-                        }
-                    ),
+                        }),
                   ],
                 );
-              }
-          );
-        }
-    );
+              });
+        });
   }
 
   Future<Map<DateTime, List<Workout>>> fetchReservationsGroupedByDate() async {
     final todayStart = DateTime.now().toUtc().toLocal();
-    final todayStartDate = DateTime(todayStart.year, todayStart.month, todayStart.day); // 오늘의 시작
-
+    final todayStartDate =
+        DateTime(todayStart.year, todayStart.month, todayStart.day); // 오늘의 시작
 
     final res = await supabase
         .from('reservations')
@@ -122,7 +120,8 @@ class _ReservationsPageState extends State<ReservationsPage> {
           )
          ''')
         .eq('user_id', supabase.auth.currentUser!.id)
-        .gte('workouts.start_time', todayStartDate.toIso8601String()); // TODO: 지난 시간을 null로 가져온다.
+        .gte('workouts.start_time',
+            todayStartDate.toIso8601String()); // TODO: 지난 시간을 null로 가져온다.
 
     List<Workout> workouts = res
         .where((e) => e['workouts'] != null) // workouts가 null인 데이터 제외
@@ -154,43 +153,4 @@ class _ReservationsPageState extends State<ReservationsPage> {
   String _formatDate(DateTime date) {
     return '${date.month}월 ${date.day}일,  ${date.year}년';
   }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
