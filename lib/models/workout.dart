5070a4a77d3cad4152d4d451124d855bd6c5a5fb
sunko <sunko@goseonghyeon-ui-MacBookPro.local>
2024-11-01 15:33:30 +0900

    Feat: 남은자리 기능 추가/#99

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -3,12 +3,14 @@ class Workout {
   final String workoutName;
   final DateTime startTime;
   final int locationId;
+  final int capacity;
 
   Workout({
     required this.id,
     required this.workoutName,
     required this.startTime,
     required this.locationId,
+    required this.capacity,
   });
 
   factory Workout.fromMap(Map<String, dynamic> map) {
@@ -17,6 +19,7 @@ class Workout {
       workoutName: map['workout_name'],
       startTime: DateTime.parse(map['start_time']),
       locationId: map['location_id'],
+      capacity: map['capacity'],
     );
   }
 }
